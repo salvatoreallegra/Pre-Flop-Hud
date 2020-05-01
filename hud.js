42143d5a9d9282344fc5f253e62daaf9302cd5da
Salvatore Allegra <salallegra@gmail.com>
2020-05-01 16:30:36 -0700

    Store and retrieve an array of opponent objects to and from localstorage

--- hud.js
+++ hud.js
@@ -1,5 +1,27 @@
 document.querySelectorAll(".box").forEach((item) => {
   item.addEventListener("click", (event) => {
-    alert("hello");
+    alert(item.innerHTML);
   });
 });
+
+var opponents = [
+  {
+    name: "whizbanger",
+    rfiHands: ["AKs", "AA"],
+    ccHands: ["99", "88"],
+    threeBetHands: ["AA", "KK"],
+  },
+  {
+    name: "Julietta",
+    rfiHands: ["AA", "AA"],
+    ccHands: ["99", "77"],
+    threeBetHands: ["AQs", "KK"],
+  },
+];
+
+function storePlayerHand(opponent) {
+  localStorage.setItem("opponent", JSON.stringify(opponent));
+}
+storePlayerHand(opponents);
+let retrievedUser = JSON.parse(localStorage.getItem("opponent"));
+console.log(retrievedUser);
