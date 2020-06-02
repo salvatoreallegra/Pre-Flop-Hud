8cc3b04897845a4587f4e23122166854ae527301
Salvatore Allegra <salallegra@gmail.com>
2020-06-01 20:28:53 -0700

    Update Main, player, and storage utils functionality

--- LocalStorageUtilsObjectLiteral.js
+++ LocalStorageUtilsObjectLiteral.js
@@ -1,38 +1,7 @@
-const localStorageKey = "players";
-const playersArray = [];
-
-//pretend I received input from the user interface
-playerName = "Venom";
-//do I create an empty object literal? or do I create a class?
-player = {
-  uuid: 0,
-  name: "",
-  vpip: 0,
-  pfr: 0,
-  totalHands: 0,
-
-  handleCall: function () {
-    this.totalHands++;
-    this.vpip++;
-  },
-  handleRaise: function () {
-    this.totalHands++;
-    this.vpip++;
-  },
-  handleFold: function () {
-    this.totalHands++;
-    this.vpip++;
-  },
-  calculateVpip: function () {
-    this.vpip = (this.vpip * 100) / this.totalHands;
-  },
-};
-player.name = playerName;
-playersArray.push(player);
-
-savePlayersToStorage(localStorageKey);
-
 function savePlayersToStorage(localStorageKey, playersArray) {
+  if (localStorage.getItem(localStorageKey === null)) {
+    localStorage.setItem(localStorageKey);
+  }
   localStorage.setItem(localStorageKey, playersArray);
 }
 
@@ -40,7 +9,7 @@ function getPlayersFromStorage(localStorageKey) {
   if (localStorage.getItem(localStorageKey === null)) {
     localStorage.setItem(localStorageKey);
   }
-  players = localStorage.getItem(JSON.parse(localStorageKey));
+  let players = JSON.parse(localStorage.getItem(localStorageKey));
 
   return players;
 }
@@ -48,3 +17,5 @@ function getPlayersFromStorage(localStorageKey) {
 function printPlayers(localStorageKey) {
   players = getPlayersFromStorage(localStorageKey);
 }
+
+export { savePlayersToStorage, getPlayersFromStorage };
--- main.js
+++ main.js
@@ -3,6 +3,24 @@
  * will call other functions
  * *******************************/
 
-function main() {}
+import { createPlayer } from "./player.js";
+import {
+  savePlayersToStorage,
+  getPlayersFromStorage,
+} from "./LocalStorageUtilsObjectLiteral.js";
+
+function main() {
+  const localStorageKey = "stored_players";
+  let players = getPlayersFromStorage(localStorageKey);
+
+  let player = createPlayer("Crusty");
+  let player2 = createPlayer("Venom");
+  let player3 = createPlayer("JackBluffer");
+
+  players.push(player);
+  players.push(player2);
+  players.push(player3);
+  savePlayersToStorage(localStorageKey, JSON.stringify(players));
+}
 
 main();
